fcc28c7793487df510559aa3f8915a48816e656c
home-wsl <codeLamb@163.com>
2026-09-05 16:35:53 +0800

    使用 wire 依赖注入

--- moddy-blog-article/internal/server/grpc.go
+++ moddy-blog-article/internal/server/grpc.go
@@ -33,14 +33,22 @@ func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, logger *slog
 	return srv
 }
 
-func NewArticleGRPCServer(service *service.ArticleService, logger *slog.Logger) *grpc.Server {
+func NewArticleGRPCServer(c *conf.Server, service *service.ArticleService, logger *slog.Logger) *grpc.Server {
 	// 创建中间件
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
 		),
 	}
-
+	if c.Grpc.Network != "" {
+		opts = append(opts, grpc.Network(c.Grpc.Network))
+	}
+	if c.Grpc.Addr != "" {
+		opts = append(opts, grpc.Address(c.Grpc.Addr))
+	}
+	if c.Grpc.Timeout != nil {
+		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
+	}
 	srv := grpc.NewServer(opts...)
 	pb.RegisterArticleServerServer(srv, service)
 	return srv
--- moddy-blog-article/internal/server/http.go
+++ moddy-blog-article/internal/server/http.go
@@ -33,12 +33,23 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger *slog
 	return srv
 }
 
-func NewArticleHTTPServer(service *service.ArticleService, logger *slog.Logger) *http.Server {
+func NewArticleHTTPServer(c *conf.Server, service *service.ArticleService, logger *slog.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
 		),
 	}
+
+	if c.Http.Network != "" {
+		opts = append(opts, http.Network(c.Http.Network))
+	}
+	if c.Http.Addr != "" {
+		opts = append(opts, http.Address(c.Http.Addr))
+	}
+	if c.Http.Timeout != nil {
+		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
+	}
+
 	src := http.NewServer(opts...)
 	pb.RegisterArticleServerHTTPServer(src, service)
 	return src
